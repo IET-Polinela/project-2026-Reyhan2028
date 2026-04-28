fd8ca0edfd093f542ab9a451080416f3c21f5af6
Reyhan2028 <sakhihanania4@gmail.com>
2026-04-28 22:29:16 +0700

    labsession_6

--- iet_2026_24782028/usermanagement_2028/admin.py
+++ iet_2026_24782028/usermanagement_2028/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+# Pengaturan tampilan untuk model User
+class UserAdmin(admin.ModelAdmin):
+    # Ini yang akan membuat kolom is_admin dan is_member muncul di daftar
+    list_display = ('username', 'email', 'is_admin', 'is_member', 'is_staff')
+    
+    # Menambahkan filter di samping kanan untuk memudahkan pencarian
+    list_filter = ('is_admin', 'is_member', 'is_staff')
+    
+    # Mengatur tampilan form saat kamu mengklik salah satu user
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Informasi Pribadi', {'fields': ('email',)}),
+        ('Status Hak Akses', {'fields': ('is_admin', 'is_member', 'is_staff', 'is_active', 'is_superuser')}),
+    )
+
+# Daftarkan model User dengan pengaturan UserAdmin di atas
+admin.site.register(User, UserAdmin)
