b5b715be7ced2e528d2ce6e1d67382ddc6ed1bbe
Reyhan2028 <sakhihanania4@gmail.com>
2026-06-28 22:47:31 +0700

    Lab 13 deployment preparation

---
+++ server_smartcity/smartcity_app/asgi.py
@@ -0,0 +1,14 @@
+"""
+ASGI config for smartcity_app project.
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'smartcity_app.settings'
+)
+
+application = get_asgi_application()
